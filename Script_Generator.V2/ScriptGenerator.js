b13d17ebe321ac1721b7c31d931e44fd4e99a9fe
AshHealy <97044868+AshHealy@users.noreply.github.com>
2022-04-11 11:23:30 +0100

    Update ScriptGenerator.js

---
+++ Script_Generator.V2/ScriptGenerator.js
@@ -0,0 +1,42 @@
+//Each section should be a separate function
+//Interior or ext, place, time
+
+function genSetting (){
+//int or ext
+const places = ["Int. ", "Ext. "]
+var place = Math.floor(Math.random() * places.length);
+let x = places[place]
+return document.getElementById("demo").innerHTML = x + " - " 
+
+}
+
+
+//Camera Direction and scene setting
+
+function genCamera (){
+
+}
+
+//First Character & Description
+
+function characterOne () {
+
+    //if Jen then - music quote responses 
+
+    //Else generic responses 
+
+
+}
+
+//Second Characrer & Descripton 
+
+function characterTwo () { 
+
+    //if Jen then - music quote responses 
+
+    //Else generic responses 
+
+
+}
+
+
