7ad7405d7f92e654fd6d3c551d48059ff3703bf6
AshHealy <97044868+AshHealy@users.noreply.github.com>
2022-06-07 11:13:23 +0100

    idk

--- GoodMorning_Generator/Iteration_01/app.js
+++ GoodMorning_Generator/Iteration_01/app.js
@@ -1,12 +1,24 @@
 function sayGoodMorning() { 
 
+    const cohort = "G31"
+    //is const the best way to store this
+    const array_greetings = [
+        " Hello there... ", "What's up ", "Ey am walkin' here ", "Syntactical sweetness ", "We need to talk ",  `I've been trying to reach you about your cars extended warranty ${cohort} ` 
+    ]
 
+    let let_greetings = Math.floor(Math.random()  * array_greetings.length);
+    let greeting = array_greetings[let_greetings]
     
+
+    const array_groupnames = [
+        "Mutants", "Troops", "Younglings", `${cohort}`, "Fire Nation", 
+    ]
+
+    let let_group = Math.floor(Math.random()  * array_groupnames.length);
+    let group = array_groupnames[let_group]
 
 
     // why use a query selector instead here?
-    document.getElementById("good_morning").innerHTML = "Hot"
-
-
-
+    //if yes, how
+    document.getElementById("good_morning").innerHTML = greeting + group + "!"
 }
