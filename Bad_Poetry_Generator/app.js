a94ab8b34b7b18f895929d840092ee7e0f47f56b
AshHealy <97044868+AshHealy@users.noreply.github.com>
2022-09-05 10:00:47 +0100

    idk what i changed here

--- Bad_Poetry_Generator/app.js
+++ Bad_Poetry_Generator/app.js
@@ -3,6 +3,8 @@ function myFunction() {
     const blues = ["I think you are smelly and your feet are too.", "I hired a lawyer and so should you.", "sometimes I jobby and sometimes I poo.", "your laces are untied and you're missing a shoe.", "have you ever wondered about the number 42?"]
     let blue = Math.floor(Math.random() * blues.length);
     let rhyme = blues[blue]
+
+    
     document.getElementById("opening").innerHTML = "Roses are red, violets are blue... " + rhyme
     
 }
